7b60836793d81dfee42bf9dae1c67d1694b02c88
kunalmalpe100 <118930100+kunalmalpe100@users.noreply.github.com>
2023-08-17 13:23:53 +0530

    Create rock paper scissors game in python.py

---
+++ rock paper scissors game in python.py
@@ -0,0 +1,41 @@
+import random
+
+def get_user_choice():
+    choice = input("Enter your choice (rock, paper, or scissors): ").lower()
+    while choice not in ["rock", "paper", "scissors"]:
+        choice = input("Invalid choice. Please choose rock, paper, or scissors: ").lower()
+    return choice
+
+def get_computer_choice():
+    return random.choice(["rock", "paper", "scissors"])
+
+def determine_winner(user_choice, computer_choice):
+    if user_choice == computer_choice:
+        return "It's a tie!"
+    elif user_choice == "rock":
+        return "You win!" if computer_choice == "scissors" else "Computer wins!"
+    elif user_choice == "paper":
+        return "You win!" if computer_choice == "rock" else "Computer wins!"
+    elif user_choice == "scissors":
+        return "You win!" if computer_choice == "paper" else "Computer wins!"
+
+def main():
+    print("Welcome to Rock-Paper-Scissors!")
+
+    while True:
+        user_choice = get_user_choice()
+        computer_choice = get_computer_choice()
+
+        print(f"You chose: {user_choice}")
+        print(f"Computer chose: {computer_choice}")
+
+        result = determine_winner(user_choice, computer_choice)
+        print(result)
+
+        play_again = input("Do you want to play again? (yes/no): ").lower()
+        if play_again != "yes":
+            print("Thanks for playing!")
+            break
+
+if __name__ == "__main__":
+    main()
